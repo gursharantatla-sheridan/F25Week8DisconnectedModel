13e689c3ed9803f805105bd2451d3915c9ce8891
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-10-31 09:23:26 -0400

    data class

---
+++ F25Week8DisconnectedModel/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace F25Week8DisconnectedModel
+{
+    public class Data
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+        }
+    }
+}
